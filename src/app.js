b1f40ddf015e121c0321084bcab4db41eab84504
Sekh Mekail Ali <skmekailali2001@gmail.com>
2026-05-29 18:37:58 +0530

    implemented get note API endpoint

--- src/app.js
+++ src/app.js
@@ -1,44 +1,80 @@
 import express from "express";
 import noteModel from "./models/note.model.js";
-import  error  from "node:console";
-// import json  from "body-parser";
 
 const app = express();
+
 app.use(express.json());
 
 /*
-@route POST /api/notes,
-@description create a new note need title and description in request body
+@route POST /api/notes
+@description Create a new note
 */
-app.post(`/api/notes`, async (req, res) => {
-  const { title, description } = req.body;
+app.post("/api/notes", async (req, res) => {
+  try {
+    const { title, description } = req.body;
 
-  // ----validations---
-  if (!title) {
-    return res.status(400).json({
-      error: `title is requierd`,
+    // validations
+    if (!title) {
+      return res.status(400).json({
+        error: "title is required",
+      });
+    }
+
+    if (!description) {
+      return res.status(400).json({
+        error: "description is required",
+      });
+    }
+
+    if (title.trim().length < 3) {
+      return res.status(400).json({
+        error: "title length must be at least 3 characters",
+      });
+    }
+
+    if (description.trim().length < 9) {
+      return res.status(400).json({
+        error: "description length must be at least 9 characters",
+      });
+    }
+
+    const newNote = await noteModel.create({
+      title,
+      description,
+    });
+
+    return res.status(201).json({
+      message: "note created successfully",
+      note: newNote,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      error: "server error",
     });
   }
+});
 
-  if (!description) {
-    return res.status(400).json({ error: `descripton is requierd` });
+/*
+@route GET /api/notes
+@description Get all notes
+*/
+app.get("/api/notes", async (req, res) => {
+  try {
+    const allNotes = await noteModel.find();
+
+    return res.status(200).json({
+      message: "fetched all notes from database",
+      notes: allNotes,
+    });
+  } catch (error) {
+    console.log(`error from database ${error}`);
+
+    return res.status(500).json({
+      error: "server error",
+    });
   }
-  if(title.trim().length<3){
-    return res.status(400),json({error:`title length must be 3`})
-  }
-
-    if(description.trim().length<9){
-    return res.status(400),json({error:`description length must be 3`})
-  }
-
-  const newNote = noteModel.create({title,description})
-
-  return res.status(201).json({
-    Message:`note created successfully`,
-    note:newNote
-  })
-
-
 });
 
 export default app;
